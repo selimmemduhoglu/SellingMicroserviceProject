6d737280b357e523a97d67f01e0f379ca17786d8
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2024-10-16 17:56:35 +0300

    completed : rabbit mq

---
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,191 @@
+﻿using EventBus.Base;
+using EventBus.Base.Events;
+using Newtonsoft.Json;
+using Polly;
+using Polly.Retry;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EventBus.RabbitMQ;
+
+public class EventBusRabbitMQ : BaseEventBus
+{
+    RabbitMQPersistentConnection persistentConnection;
+    private readonly IConnectionFactory connectionFactory;
+    private readonly IModel consumerChannel;
+    public EventBusRabbitMQ(IServiceProvider serviceProvider, EventBusConfig config) : base(serviceProvider, config)
+    {
+        if (config.Connection != null)
+        {
+            var connJson = JsonConvert.SerializeObject(EventBusConfig.Connection, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            connectionFactory = JsonConvert.DeserializeObject<ConnectionFactory>(connJson);
+        }
+        else
+            connectionFactory = new ConnectionFactory();
+
+        persistentConnection = new RabbitMQPersistentConnection(connectionFactory, config.ConnectionRetryCount);
+
+        consumerChannel = CreateConsumerChannel();
+
+        SubsManager.OnEventRemoved += SubsManager_OnEventRemoved;
+    }
+
+
+    #region Public Method
+
+    public override void Publish(IntegrationEvent @event)
+    {
+        if (!persistentConnection.isConnection)
+        {
+            persistentConnection.TryConnect();
+        }
+
+        RetryPolicy policy = Policy.Handle<BrokerUnreachableException>()
+            .Or<SocketException>()
+            .WaitAndRetry(EventBusConfig.ConnectionRetryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+            {
+                // log
+
+            });
+
+
+        string eventName = @event.GetType().Name;
+        eventName = ProcessEventName(eventName);
+
+        consumerChannel.ExchangeDeclare(exchange: EventBusConfig.DefaultTopicName, type: "direct"); // BUrada exchange in olmama ihtimaline karşılık create ed,l,yor.
+
+        string message = JsonConvert.SerializeObject(@event);
+
+        byte[] body = Encoding.UTF8.GetBytes(message);
+
+        policy.Execute(() =>
+        {
+            var properties = consumerChannel.CreateBasicProperties();
+
+            properties.DeliveryMode = 2; // persistent
+
+            consumerChannel.QueueDeclare(queue: GetSubName(eventName), // Queue'nun create edilip edilmediği bilgisi için. Create edilmediyse create edecek.
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+            consumerChannel.BasicPublish(
+                     exchange: EventBusConfig.DefaultTopicName,
+                     routingKey: eventName,
+                     mandatory: true,
+                     basicProperties: properties,
+                     body: body);
+
+        });
+
+    }
+    public override void Subscribe<T, TH>()
+    {
+        string eventName = typeof(T).Name;
+        eventName = ProcessEventName(eventName);
+
+        if (!SubsManager.HasSubscriptionsForEvent(eventName))
+        {
+            if (!persistentConnection.isConnection)  // Connection bağlı değilse bağlanıyor.
+            {
+                persistentConnection.TryConnect();
+            }
+
+            consumerChannel.QueueDeclare(queue: GetSubName(eventName), // Burada queue create edilir. (burada consume edeceğimiz queue nin daha önceden oluşturulup oluşturulmadığı bilgisini almak.)
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            consumerChannel.QueueBind(queue: GetSubName(eventName), // burada queue baplanır. (burada exchange ve queue bind ediliyor.)
+                exchange: EventBusConfig.DefaultTopicName,
+                routingKey: eventName);
+        }
+
+        SubsManager.AddSubscription<T, TH>();
+        StartBasicConsume(eventName);
+    }
+    public override void UnSubscribe<T, TH>()
+    {
+        SubsManager.RemoveSubscription<T, TH>();
+    }
+
+    #endregion
+
+
+    #region Private Method
+
+    private void SubsManager_OnEventRemoved(object sender, string eventName)
+    {
+        eventName = ProcessEventName(eventName);
+
+        if (!persistentConnection.isConnection)
+        {
+            persistentConnection.TryConnect();
+        }
+        // Daha önceden bind ettiğimiz wueue yu UnBind et.
+        consumerChannel.QueueUnbind(queue: eventName,
+            exchange: EventBusConfig.DefaultTopicName,
+            routingKey: eventName);
+
+
+        if (SubsManager.IsEmpty)
+        {
+            consumerChannel.Close();
+        }
+    }
+    private IModel CreateConsumerChannel()
+    {
+        if (!persistentConnection.isConnection) // Connection ımızın connectred olup olmadığına bakıp oan göre bağlanıyoruz.
+        {
+            persistentConnection.TryConnect();
+        }
+
+        IModel channel = persistentConnection.CreateModel(); // Bağlanma durumuna göre bir channel oluşturulur ve Model return eder.
+        channel.ExchangeDeclare(exchange: EventBusConfig.DefaultTopicName, type: "direct");
+
+        return channel;
+    }
+    private void StartBasicConsume(string eventName)
+    {
+        if (consumerChannel != null)
+        {
+            EventingBasicConsumer consumer = new EventingBasicConsumer(consumerChannel);
+
+            consumer.Received += Consumer_Received;
+
+            consumerChannel.BasicConsume(
+                queue: GetSubName(eventName),
+                autoAck: false,
+                consumer: consumer);
+        }
+    }
+    private async void Consumer_Received(object sender, BasicDeliverEventArgs eventArgs)
+    {
+        string eventName = eventArgs.RoutingKey;
+        eventName = ProcessEventName(eventName);
+        string message = Encoding.UTF8.GetString(eventArgs.Body.Span);
+
+        try
+        {
+            await ProcessEvent(eventName, message);
+        }
+        catch (Exception ex)
+        {
+            // logging
+        }
+
+        consumerChannel.BasicAck(eventArgs.DeliveryTag, multiple: false);
+    }
+
+    #endregion
+
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/RabbitMQPersistentConnection.cs
@@ -0,0 +1,83 @@
+﻿using Polly;
+using Polly.Retry;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Net.Sockets;
+
+namespace EventBus.RabbitMQ;
+
+public class RabbitMQPersistentConnection : IDisposable
+{
+    IConnectionFactory connectionFactory;
+    private readonly int retryCount; // bağlanmak için kaç kere denesin diye
+    private IConnection connection; // hangi conenction ın açık olup  olmadığı bilgisini tutmak için
+    private object lock_object = new object();  // Bu her conenction create edildiğinde gelecek.
+    private bool _dispose;
+
+    public RabbitMQPersistentConnection(IConnectionFactory connectionFactory, int retryCount = 5)
+    {
+        this.connectionFactory = connectionFactory;
+        this.retryCount = retryCount;
+    }
+    public bool isConnection => connection != null && connection.IsOpen; // Bu 2 şartı sağlıyorsa true dönebilir problem yok. (O an connection ın aktif olup olmadığı bilgisini dönüyor.) - (Connection un olup ve açık olması durumuna göre bakılıyor.)
+
+    public IModel CreateModel()
+    {
+        return connection.CreateModel();
+    }
+    public void Dispose()
+    {
+        _dispose = true; // bunun amacı aşağıda yapılan Retry mekanizmaları çalıştığında dispose edilip edilmemsinin kontolünü yapıyoruz.
+        connection.Dispose();
+    }
+    public bool TryConnect() // Bu yapı Retry mekanizması kurmaya yarıyor.
+    {
+        // Kaç kere deneyeceği parametre olarak verildi bu yüzden recursive bir şey olmayacak ve sonsuza kadar gitmeyecek. Sınırlı bir tekrar etme mekanizması oluşturuldu.
+         
+        lock (lock_object) // lock_object'sini kilitledik. Burada aynı method çağrıldığında önce ki methodun bitmeisni bekleyecek.
+        {
+            RetryPolicy policy = Policy.Handle<SocketException>()
+                              .Or<BrokerUnreachableException>()
+                              .WaitAndRetry(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+                              {
+                              }
+
+                            );
+
+            policy.Execute(() =>
+            {
+                connection = connectionFactory.CreateConnection();
+            });
+
+
+            if (isConnection)
+            { // Burada ki eventler RabbitMQ nun eventleri
+                connection.ConnectionShutdown += Connection_ConnectionShutdown;
+                connection.CallbackException += Connection_CallbackException;
+                connection.ConnectionBlocked += Connection_ConnectionBlocked;
+                //log
+                return true;
+            }
+            return false;
+        }
+    }
+
+    private void Connection_ConnectionShutdown(object sender, ShutdownEventArgs e)
+    {
+        if (_dispose) return; // Eğer ki dispose edildiyse denemesin ve geri gitsin diye yazdık.
+                              // log Connection_ConnectionShutdown
+        TryConnect(); // Tekrar TryConnect yapısını çağırıp tekrar denemesi için
+    }
+    private void Connection_ConnectionBlocked(object sender, ConnectionBlockedEventArgs e)
+    {
+        if (_dispose) return;
+        TryConnect();
+    }
+    private void Connection_CallbackException(object sender, CallbackExceptionEventArgs e)
+    {
+        if (_dispose) return;
+        TryConnect();
+    }
+}
