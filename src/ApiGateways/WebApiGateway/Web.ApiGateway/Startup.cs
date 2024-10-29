1887c9422cac3cd9ec8454c922a555a32e10cd46
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2024-10-29 17:33:12 +0300

    updated : consul service registiration

--- src/ApiGateways/WebApiGateway/Web.ApiGateway/Startup.cs
+++ src/ApiGateways/WebApiGateway/Web.ApiGateway/Startup.cs
@@ -59,7 +59,7 @@ namespace Web.ApiGateway
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -91,7 +91,7 @@ namespace Web.ApiGateway
             app.UseHealthChecksUI(config => config.UIPath = "/hc-ui");
 
 
-            await app.UseOcelot();
+            app.UseOcelot().GetAwaiter().GetResult();
         }
 
         private void ConfigureHttpClient(IServiceCollection services)
--- src/Services/CatalogService/CatalogService.Api/Extensions/ConsulRegistration.cs
+++ src/Services/CatalogService/CatalogService.Api/Extensions/ConsulRegistration.cs
@@ -1,15 +1,10 @@
 ﻿using Consul;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting.Server.Features;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CatalogService.Api.Extensions
 {
