56f563101e0cb729dfa0df0577ad76a52eb40dd3
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2024-12-28 16:02:31 +0300

    updated

--- src/Clients/BlazorWebApp/WebApp/Application/Services/CatalogService.cs
+++ src/Clients/BlazorWebApp/WebApp/Application/Services/CatalogService.cs
@@ -1,29 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using WebApp.Application.Services.Interfaces;
+﻿using WebApp.Application.Services.Interfaces;
 using WebApp.Domain.Models;
 using WebApp.Domain.Models.CatalogModels;
 using WebApp.Extensions;
 
 namespace WebApp.Application.Services
 {
-    public class CatalogService : ICatalogService
-    {
-        private readonly HttpClient apiClient;
+	public class CatalogService : ICatalogService
+	{
+		private readonly HttpClient apiClient;
 
-        public CatalogService(HttpClient apiClient)
-        {
-            this.apiClient = apiClient;
-        }
+		public CatalogService(HttpClient apiClient)
+		{
+			this.apiClient = apiClient;
+		}
 
-        public async Task<PaginatedItemsViewModel<CatalogItem>> GetCatalogItems()
-        {
-            var response = await apiClient.GetResponseAsync<PaginatedItemsViewModel<CatalogItem>>("/catalog/items");
+		public async Task<PaginatedItemsViewModel<CatalogItem>> GetCatalogItems()
+		{
+			var response = await apiClient.GetResponseAsync<PaginatedItemsViewModel<CatalogItem>>("/catalog/items");
 
-            return response;
-        }
-    }
+			return response;
+		}
+	}
 }
