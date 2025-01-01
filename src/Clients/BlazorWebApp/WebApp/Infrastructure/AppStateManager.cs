c6c3cf73ded8ba68d14cc1432dfb77b7212e5432
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2025-01-01 21:51:26 +0300

    updated

--- src/Clients/BlazorWebApp/WebApp/Infrastructure/AppStateManager.cs
+++ src/Clients/BlazorWebApp/WebApp/Infrastructure/AppStateManager.cs
@@ -1,24 +1,20 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApp.Infrastructure
 {
-    public class AppStateManager
-    {
-        public event Action<ComponentBase, string> StateChanged;
+	public class AppStateManager
+	{
+		public event Action<ComponentBase, string> StateChanged;
 
-        public void UpdateCart(ComponentBase component)
-        {
-            StateChanged?.Invoke(component, "updatebasket");
-        }
+		public void UpdateCart(ComponentBase component)
+		{
+			StateChanged?.Invoke(component, "updatebasket");
+		}
 
-        public void LoginChanged(ComponentBase component)
-        {
-            StateChanged?.Invoke(component, "login");
-        }
+		public void LoginChanged(ComponentBase component)
+		{
+			StateChanged?.Invoke(component, "login");
+		}
 
-    }
+	}
 }
